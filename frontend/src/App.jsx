3ec0d9640f18d6e1526b23c3c59cc4d8011bf3e8
StephOg <stephanie.ogwo@gmail.com>
2024-08-14 14:35:29 -0400

    drafted initialFetch and updated landing page and CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,14 +15,37 @@ function App() {
   const apiUrl = process.env.VITE_API_URL;
   const apiKey = process.env.VITE_API_KEY;
 
-  const [user, setUser] = useState("");
+
+  const [logs, setLogs] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const initialFetch = async () => {
+      try {
+        const usersResponse = await fetch(`${apiUrl}/users`);
+        const logsResponse = await fetch(`${apiUrl}/logs/expand`);
+
+        const usersData = await usersResponse.json();
+        const logsData = await logsResponse.json();
+
+        setUsers(usersData);
+        setLogs(logsData);
+
+        const latestUser = usersData.reduce((prev, current) => (prev.id > current.id ? prev : current));
+        setUser(latestUser);
+      } catch (error) {
+        console.error("Error fetching users and logs:", error);
+      }
+    };
+    fetchUsersAndLogs();
+  }, [apiUrl]);
 
   return (
     <>
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<ExpandedLog />} />
+          <Route path="/" element={<ExpandedLog users={users} logs={logs} user={user} setUser={setUser} />} />
           <Route path="/add/log" element={<AddNew />} />
           <Route path="/add/user" element={<AddNew />} />
           <Route path="/add/food" element={<AddNew />} />
--- frontend/src/pages/ExpandedLog.jsx
+++ frontend/src/pages/ExpandedLog.jsx
@@ -4,6 +4,10 @@ import UserHeader from "../components/UserHeader";
 import React from 'react'
 
 function ExpandedLog({ users, user, logs, setUser }) {
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <UserHeader user={user} />
